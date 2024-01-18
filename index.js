1361b3b2eeffff3e6c7d123049bb2370abb97459
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-18 10:04:07 +0100

    change route to access

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
+app.get('/api/v1', (req, res) => {
     res.send(workers)
 })
 
-app.get('/menores-de-30', (req, res) => {
+app.get('/api/v1/menores-de-30', (req, res) => {
     let menores = workers.filter(worker => worker.age <= 30)
     res.send(menores)
 })
 
-app.get('/edad/:edad', (req, res) => {
+app.get('/api/v1/edad/:edad', (req, res) => {
     let edad = req.params.edad
     let filtradors = workers.filter(worker => worker.age == edad)
     res.send(filtradors)
